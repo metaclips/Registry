7d23bc94edeb7408d3d75c07de8e7a32aa93f315
Michael Uti <utimichael9@gmail.com>
2021-09-09 20:45:44 +0000

    Update bash script

--- .sh
+++ .sh
@@ -6,17 +6,25 @@ echo -n $'[registries]\nmy-registry = { index = "http://127.0.0.1:3333/git" }\n\
 mkdir vendor
 cargo install cargo-local-registry
 
-git clone https://github.com/d-e-s-o/cargo-http-registry.git && cd cargo-http-registry
+git clone https://github.com/d-e-s-o/cargo-http-registry.git
+
+cd cargo-http-registry
 
 git checkout topic/symlink
 
 cargo build --release
 
-./target/release/cargo-http-registry ../vendor  --addr="127.0.0.1:3333" &
+git config --global user.email "notused@email.com"
+git config --global user.name "Ockam"
 
+./target/release/cargo-http-registry add/vendor/full/path/here  --addr="127.0.0.1:3333" &
+
+echo "Started http registry"
 cd ../foo
 # Download all dependencies to our local
 cargo local-registry --sync Cargo.lock ../vendor --no-delete >> ../.cargo/config
+echo "Retrieved dependencies from crates.io"
+
 cargo publish --token null
 
 
